465221fda99df102f15907a639cf951883167791
keiukunnarpuu <148068827+keiukunnarpuu@users.noreply.github.com>
2024-05-05 22:27:08 +0300

    simple_class.py

---
+++ simple_class.py
@@ -0,0 +1,14 @@
+"""Simple class."""
+
+class Student:
+    """Student class."""
+
+    def __init__(self, name):
+        """Student class construct.
+
+        Creates student type object.
+        Parameters:
+        n (str): Name of student.
+        """
+        self.name = name
+        self.finished = False
